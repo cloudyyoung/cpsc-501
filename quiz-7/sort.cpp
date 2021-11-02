6795d97fd71d7132e371072f69e269a264180dce
Cloudy Young <cloudy.young@outlook.com>
2021-11-01 23:04:37 -0600

    Update quiz-7

--- quiz-7/sort.cpp
+++ quiz-7/sort.cpp
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <time.h>
 #include <vector>
+#include <cstdlib>
 #include <array>        // std::array
 #include "helpers.h"	// functions to print, shuffle, etc.
 
+int merge_count = 0;
+int merge_fail_return = 0;
 
 void mergesort(InputArray& array, int start, int end) {
+	merge_count ++;
 	
 	if (start >= end) {
+		merge_fail_return++;
 		return;
 	}
-	
+
 	int mid = start + (end - start) / 2;	//halfway point
-	
-	// recursively sort the two halves
-	mergesort(array, start, mid);
-	mergesort(array, mid+1, end);
+
+	if(start < mid){ // Only when there is more than one element in the first half
+		mergesort(array, start, mid);
+	}
+
+	if(mid + 1 < end){ // Only when there is more than one element in the second half
+		mergesort(array, mid+1, end);
+	}
 	
 	merge(array, start, mid, end);
 }
@@ -23,7 +32,7 @@ void mergesort(InputArray& array, int start, int end) {
 
 void quicksort(InputArray& array, int start, int end) {
 	
-    if (start <= end) {
+    if (start < end) {
 		// p is the index of the correctly placed pivot
         int p = partition(array, start, end);	// chooses the first element as the pivot
  
@@ -53,14 +62,17 @@ int main() {
 	before = clock();
 	mergesort(array, 0, array.size()-1);	
 	totalMerge = clock() - before;	
-	//printArray(array);
+	// printArray(array);
 
 	// time the second sorting algorithm
-	before = clock();
-	shuffleArray(array2);
-	quicksort(array2, 0, array2.size()-1);
-	totalQuick = clock() - before;
+	// before = clock();
+	// shuffleArray(array2);
+	// quicksort(array2, 0, array2.size()-1);
+	// totalQuick = clock() - before;
 	//printArray(array2);
+
+	printf("merge count %d\n", merge_count);
+	printf("merge fail %d\n", merge_fail_return);
 	
 	
 	// display results
