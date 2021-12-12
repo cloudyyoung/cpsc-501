22366cb0bb9cb1784fbd1f3fcc0faebc505dc57d
Cloudy Young <cloudy.young@outlook.com>
2021-12-12 11:29:30 -0700

    Update heart_starter.py

--- assignment-4/heart_starter.py
+++ assignment-4/heart_starter.py
@@ -22,13 +22,71 @@ def standardize(x, mu, sigma):
     return ((x - mu)/sigma)
     
 
+
 ##############################################
+
+
+# given a list with the features and label for a sample (row of the csv),
+# converts it to a numeric feature vector and an integer label
+# returns the tuple (feature, label)
+def getDataFromSample(sample):
+
+    # sbp
+    sbp = cv([standardize(int(sample[1]), 138.3, 20.5)])
+
+    # tobacco
+    tobacco = cv([standardize(float(sample[2]), 3.64, 4.59)])
+
+    # ldl
+    ldl = cv([standardize(float(sample[3]), 4.74, 2.07)])
+
+    # adiposity
+    adiposity = cv([standardize(float(sample[4]), 25.4, 7.77)])
+
+    # famhist
+    if(sample[5] == 'Present'):
+        famhist = cv([1])
+    else:
+        famhist = cv([0])
+
+    # typea
+    typea = cv([standardize(int(sample[6]), 53.1, 9.81)])
+
+    # obesity
+    obesity = cv([standardize(float(sample[7]), 26.0, 4.21)]
+)
+    # alcohol
+    alcohol = cv([standardize(float(sample[8]), 17.0, 24.5)])
+
+    # age
+    age = cv([standardize(int(sample[9]), 42.8, 14.6)])
+
+    features = np.concatenate((sbp, tobacco, ldl, adiposity, famhist, typea, obesity, alcohol, age), axis=0)
+
+    # chd (label)
+    label = int(sample[10])
+
+    return (features, label)
 
 # reads number of data points, feature vectors and their labels from the given file
 # and returns them as a tuple
 def readData(filename):
 
-    # CODE GOES HERE
+    with open(filename, newline='') as datafile:
+        reader = csv.reader(datafile)
+        next(reader, None)  # skip the header row
+
+        n = 0
+        features = []
+        labels = []
+
+        for row in reader:
+            featureVec, label = getDataFromSample(row)
+            features.append(featureVec)
+            labels.append(label)
+            n = n + 1
+
+    print(f"Number of data points read: {n}")
     
     return n, features, labels
 
@@ -41,10 +99,25 @@ def readData(filename):
 def prepData():
 
     n, features, labels = readData('data/heart.csv')
+    
 
-    # CODE GOES HERE
+    ntrain = int(n * 5/6)
+    ntest = n - ntrain
 
+    # split into training and testing data
+    trainingFeatures = features[:ntrain]
+    trainingLabels = [onehot(label, 2) for label in labels[:ntrain]]    # training labels should be in onehot form
+
+    print(f"Number of training samples: {ntrain}")
+
+    testingFeatures = features[ntrain:]
+    testingLabels = labels[ntrain:]
+    print(f"Number of testing samples: {ntest}")
+
+    trainingData = zip(trainingFeatures, trainingLabels)
+    testingData = zip(testingFeatures, testingLabels)
     return (trainingData, testingData)
+
 
 
 ###################################################
