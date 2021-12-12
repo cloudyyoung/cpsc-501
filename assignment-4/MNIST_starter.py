c448da24f19fa1ec045d3d28b7f376c448e80902
Cloudy Young <cloudy.young@outlook.com>
2021-12-12 09:20:50 -0700

    Update MNIST_starter.py

--- assignment-4/MNIST_starter.py
+++ assignment-4/MNIST_starter.py
@@ -5,12 +5,12 @@ import network
 # converts a 1d python list into a (1,n) row vector
 def rv(vec):
     return np.array([vec])
-    
+
 # converts a 1d python list into a (n,1) column vector
 def cv(vec):
     return rv(vec).T
-        
-# creates a (size,1) array of zeros, whose ith entry is equal to 1    
+
+# creates a (size,1) array of zeros, whose ith entry is equal to 1
 def onehot(i, size):
     vec = np.zeros(size)
     vec[i] = 1
@@ -34,25 +34,26 @@ testingLabelFile = "data/t10k-labels.idx1-ubyte"
 def getLabels(labelfile):
     file = open(labelfile, 'rb')
     file.read(4)
-    n = int.from_bytes(file.read(4), byteorder='big') # number of entries
-    
+    n = int.from_bytes(file.read(4), byteorder='big')  # number of entries
+
     labelarray = bytearray(file.read())
     labelarray = [b for b in labelarray]    # convert to ints
     file.close()
-    
+
     return n, labelarray
 
 # returns a list containing the pixels for each image, stored as a (784, 1) numpy array
 def getImgData(imagefile):
     # returns an array whose entries are each (28x28) pixel arrays with values from 0 to 255.0
-    images = idx2numpy.convert_from_file(imagefile) 
-    
+    images = idx2numpy.convert_from_file(imagefile)
+
     # We want to flatten each image from a 28 x 28 to a 784 x 1 numpy array
     # CODE GOES HERE
-    
+    features = []
+
     # convert to floats in [0,1] (only really necessary if you have other features, but we'll do it anyways)
     # CODE GOES HERE
-   
+
     return features
 
 
@@ -61,21 +62,24 @@ def getImgData(imagefile):
 # returns a tuple (trainingData, testingData), each of which is a zipped array of features and labels
 def prepData():
     ntrain, train_labels = getLabels(trainingLabelFile)
+    print("Training data:", ntrain, "entries")
+    print("Training labels:", len(train_labels), "entries")
 
     # CODE GOES HERE
-       
+    trainingData = train_labels[:50000]
+    testingData = train_labels[50000:]
+
     return (trainingData, testingData)
-    
+
 
 ###################################################
 
 
 trainingData, testingData = prepData()
 
-net = network.Network([784,30,10])
-net.SGD(trainingData, 10, 10, 1, test_data = testingData)
+print("Training data:", len(trainingData))
+print("Testing data:", len(testingData))
 
 
-
-
-        
+# net = network.Network([784, 30, 10])
+# net.SGD(trainingData, 10, 10, 1, test_data=testingData)
